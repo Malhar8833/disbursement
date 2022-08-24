f1ac15c5efb154c536c3dc657e06fe65f5413a92
Malhar8833 <malharurade@gmail.com>
2022-08-24 20:59:54 +0530

    disbursement

---
+++ src/main/java/com/pensionerDisbursementMicroservice/Model/ProcessPensionResponse.java
@@ -0,0 +1,38 @@
+package com.pensionerDisbursementMicroservice.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+/*@NoArgsConstructor
+@AllArgsConstructor*/
+@SuppressWarnings("unused")
+@Getter
+@Setter
+@EqualsAndHashCode
+public class ProcessPensionResponse {
+
+	public ProcessPensionResponse() {
+		//super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProcessPensionResponse(int pensionStatusCode) {
+		super();
+		this.pensionStatusCode = pensionStatusCode;
+	}
+
+	public int getPensionStatusCode() {
+		return pensionStatusCode;
+	}
+
+	public void setPensionStatusCode(int pensionStatusCode) {
+		this.pensionStatusCode = pensionStatusCode;
+	}
+
+	private int pensionStatusCode;
+
+}
